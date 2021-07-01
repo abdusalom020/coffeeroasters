030ccfee924388349734ede7830267a59fd1a8fb
abdusalom020 <acridsss@gmail.com>
2021-07-02 01:11:10 +0500

    plans

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ if (elsHeaderBtn){
     elsHeader.classList.toggle('side-header--open');
   });
 }
+
+var elsRightFormItem = document.querySelectorAll(".right-form__item");
+var elsRightHeaderBtn = document.querySelectorAll(".right-header__btn");
+
+elsRightHeaderBtn.forEach(function (open) {
+  open.addEventListener('click', function () {
+    open.closest('.right-form__item').classList.toggle('right-form__item--active');
+  })
+})
